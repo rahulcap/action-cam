1e55572abf30d5d93b3c51055563609b1efe5602
Rahul C <rahulcap@gmail.com>
2018-10-17 15:15:26 -0700

    attempt to use built-in tracker

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import argparse
+from tracker.tracker import ActionTracker
 
 if __name__ == '__main__':
 
@@ -33,7 +34,15 @@ if __name__ == '__main__':
         print 'Cannot read video file'
         exit()
 
+    # init tracker
+    tracker = ActionTracker()
+    # ball_rect = (456, 148, 12, 12) # SD
+    ball_rect = (908, 295, 29, 25) # HD
+    # ballrect =
+    tracker.init(frame, ball_rect=ball_rect)
+
     # Tracking loop
+    count = 1
     while(True):
         # Capture frame-by-frame
         ok, frame = video.read()
@@ -41,14 +50,33 @@ if __name__ == '__main__':
             print 'Done reading video file'
             break
 
+        # update tracker
+        ok = tracker.update(frame)
+        count += 1
+        if not ok:
+            print 'on frame {}, failed to track'.format(count)
+            break
+
+        # draw bounding box
+        rect = tracker.ball_rect
+        cv.rectangle(frame, (int(rect[0]), int(rect[1])), (int(rect[0] + rect[2]), int(rect[1] + rect[3])), (255, 0, 0), 2, 1)
+
+
         # Display the resulting frame
         cv.imshow('tracking',frame)
 
-        # check if user quit, by pressing 'q' or ESC
+        # wait between frames to show at 'normal' speed
         wait_time = int(900 / fps)
+
+        # check if user quit, by pressing 'q' or ESC
         key = cv.waitKey(wait_time) & 0xff
         if key == ord('q') or key == 27:
             break
+
+        # write frame out to file
+        # cv.imwrite('videos/frame.png', frame)
+        # exit()
+
 
     # When everything done, release the capture
     video.release()
--- tracker/tracker.py
+++ tracker/tracker.py
@@ -1,23 +1,66 @@
 import cv2 as cv
 
 
-def create_tracker(tracker_type):
-    if tracker_type == 'BOOSTING':
-        return cv.TrackerBoosting.create()
-    elif tracker_type == 'MIL':
-        return cv.TrackerMIL_create()
-    elif tracker_type == 'KCF':
-        return cv.TrackerKCF_create()
-    elif tracker_type == 'TLD':
-        return cv.TrackerTLD_create()
-    elif tracker_type == 'MEDIANFLOW':
-        return cv.TrackerMedianFlow_create()
-    elif tracker_type == 'GOTURN':
-        return cv.TrackerGOTURN_create()
-    elif tracker_type == 'MOSSE':
-        return cv.TrackerMOSSE_create()
-    elif tracker_type == "CSRT":
-        return cv.TrackerCSRT_create()
-    else:
-        raise ValueError('Invalid param: tracker_type({})'.format(tracker_type))
+class ActionTracker(object):
+    # vars
+    _tracker = None
+    _ball_rect = None
+    _frame = None
+
+    ##
+    # constructor
+    #
+    def __init__(self, tracker_type='KCF'):
+        if tracker_type == 'BOOSTING':
+            self._tracker = cv.TrackerBoosting_create()
+        elif tracker_type == 'MIL':
+            self._tracker = cv.TrackerMIL_create()
+        elif tracker_type == 'KCF':
+            self._tracker = cv.TrackerKCF_create()
+        elif tracker_type == 'TLD':
+            self._tracker = cv.TrackerTLD_create()
+        elif tracker_type == 'MEDIANFLOW':
+            self._tracker = cv.TrackerMedianFlow_create()
+        elif tracker_type == 'GOTURN':
+            self._tracker = cv.TrackerGOTURN_create()
+        elif tracker_type == 'MOSSE':
+            self._tracker = cv.TrackerMOSSE_create()
+        elif tracker_type == 'CSRT':
+            self._tracker = cv.TrackerCSRT_create()
+        else:
+            raise ValueError('Invalid tracker_type: {}'.format(tracker_type))
+
+    ##
+    # properties
+    #
+    @property
+    def ball_rect(self):
+        return self._ball_rect
+
+    ##
+    # init tracker
+    #
+    def init(self, frame, ball_rect = None):
+        self._frame = frame
+        if ball_rect is not None:
+            self._ball_rect = ball_rect
+        # TODO scan image for best rect
+        if self._ball_rect is not None:
+            ok = self._tracker.init(frame, self._ball_rect)
+            if not ok:
+                raise Exception('Unable to init tracker')
+
+    ##
+    # update tracker
+    #
+    def update(self, frame):
+        self._frame = frame
+        ok, rect = self._tracker.update(frame)
+        if ok:
+            self._ball_rect = rect
+        else:
+            # TODO log failures to track
+            print 'failed to track ball'
+
+        return ok
 
